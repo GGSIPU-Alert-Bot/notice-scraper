dc5c09ba37531357bee9988f9cb0b4d1be8b44f5
shubhsardana29 <shubhsardana29@gmail.com>
2024-08-01 15:32:23 +0530

    Refactor cron job to run every hour from 8 AM to 10 PM

--- src/app.ts
+++ src/app.ts
@@ -12,13 +12,8 @@ app.get('/', (_, res) => {
   res.json({ message: 'Welcome, Services are running!' });
 });
 
-//cron jobs to run at 8 AM, 11AM, 1 PM, 3 PM, 5 PM, and 10 PM every day.
-new CronJob('0 8 * * *', checkAndUpdateNotices, null, true);
-new CronJob('0 11 * * *', checkAndUpdateNotices, null, true);
-new CronJob('0 13 * * *', checkAndUpdateNotices, null, true);
-new CronJob('0 15 * * *', checkAndUpdateNotices, null, true);
-new CronJob('0 17 * * *', checkAndUpdateNotices, null, true);
-new CronJob('0 22 * * *', checkAndUpdateNotices, null, true);
+// Cron job to run every hour starting from 8 AM to 10 PM every day.
+new CronJob('0 8-22 * * *', checkAndUpdateNotices, null, true);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
