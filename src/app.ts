68e3d8b97ce62bb134340f6c5e592911f3dfc975
shubhsardana29 <shubhsardana29@gmail.com>
2024-08-02 22:41:35 +0530

    Refactor app.ts to add JSON body parsing middleware and start the server

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,10 @@ import { checkAndUpdateNotices } from './controllers/notice.controller';
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Route handling for notices
 app.use('/notices', noticeRoutes);
 
 app.get('/', (_, res) => {
@@ -15,6 +19,7 @@ app.get('/', (_, res) => {
 // Cron job to run at the start of every hour every day.
 new CronJob('0 * * * *', checkAndUpdateNotices, null, true);
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
